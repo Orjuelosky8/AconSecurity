90a1b5062a606b4c77ba428e6a13e44677acb8fd
maikol vergara <afiliaciones.tuapo@gmail.com>
2025-07-09 22:58:38 +0000

    giralo para que se vea de frente

--- src/components/landing/Hero.tsx
+++ src/components/landing/Hero.tsx
@@ -29,8 +29,7 @@ export default function Hero() {
         '/models/Logo Acon 3d.glb',
         (gltf) => {
             model = gltf.scene;
-            model.scale.set(0.05, 0.05, 0.05); // Hice el modelo más pequeño
-            model.rotation.x = Math.PI / 2;
+            model.scale.set(0.015, 0.015, 0.015);
             
             // Cambié el material a un color dorado metálico
             const goldMaterial = new THREE.MeshStandardMaterial({
